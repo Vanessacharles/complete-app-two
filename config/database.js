b826ee44c3545639574e814288ad02f49743c50b
Vanessa Charles <vanessacharles@Vanessas-MacBook-Air.local>
2020-03-23 08:50:28 -0400

    first commiy

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://nessa:shoes@cluster0-szbez.mongodb.net/test?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'demo'
+};
